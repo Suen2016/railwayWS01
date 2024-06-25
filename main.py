a6a59196dc75008fd09c52177adc6e227112b940
suen2016 <suenodesign2016@gmail.com>
2024-06-25 23:30:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+from fastapi import FastAPI, WebSocket
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
